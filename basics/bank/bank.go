4a2f3d4998919efa57a9138602976a05a74c554d
mehmetcan.tanriverdi <mehmetcan.tanriverdi@ddtech.com.tr>
2024-04-24 18:08:00 +0300

    loop

--- basics/bank/bank.go
+++ basics/bank/bank.go
@@ -8,35 +8,51 @@ func main() {
 	var accountBalance = 1000.0
 	var depositAmount float64
 	var withdrawAmount float64
-	fmt.Println("welcome th Go Bank")
-	fmt.Println("Choose an Operation!")
-	fmt.Println(
-		`
-		1. Check Balance 
-		2. Deposit Money
-		3. Withdraw Money
-		4. Exit
-		`)
+
+	for i :=0 ; i < 2; i++ {
+
 	
-	var choise int 
-	fmt.Print("Choise an operation:" )
-	fmt.Scan(&choise)
-	fmt.Println("Your Choise : ", choise)
+		fmt.Println("welcome th Go Bank")
+		fmt.Println("Choose an Operation!")
+		fmt.Println(
+			`
+			1. Check Balance 
+			2. Deposit Money
+			3. Withdraw Money
+			4. Exit
+			`)
+		
+		var choise int 
+		fmt.Print("Choise an operation:" )
+		fmt.Scan(&choise)
+		fmt.Println("Your Choise : ", choise)
 
 
-	if choise == 1 {
-		fmt.Println("Your Balance is", accountBalance)
-	} else if choise == 2 {
-		fmt.Print("Your Deposit: ")
-		fmt.Scan(&depositAmount)
-		accountBalance += depositAmount
-		fmt.Println("New Amount: ", accountBalance )
-	} else if choise == 3 {
-		fmt.Print("Enter the amount to Withdraw: ")
-		fmt.Scan(&withdrawAmount)
-		accountBalance -= withdrawAmount
-		fmt.Println("New Amount: ", accountBalance )
-	} else {
-		fmt.Println("Exiting the Bank. Thank You! " )
+		if choise == 1 {
+			fmt.Println("Your Balance is", accountBalance)
+		} else if choise == 2 {
+			fmt.Print("Your Deposit: ")
+			fmt.Scan(&depositAmount)
+			if depositAmount <= 0 {
+				fmt.Println("The Deposit amount should be greater than 0!")
+				return
+			}
+			accountBalance += depositAmount
+			fmt.Println("New Amount: ", accountBalance )
+		} else if choise == 3 {
+			fmt.Print("Enter the amount to Withdraw: ")
+			fmt.Scan(&withdrawAmount)
+			if withdrawAmount <= 0  {
+				fmt.Println("The Withdraw amount should be greater than 0!")
+				return
+			} else if withdrawAmount <= accountBalance{
+				fmt.Println("The Withdraw amount should not be greater than account balance!")
+				return
+			}
+			accountBalance -= withdrawAmount
+			fmt.Println("New Amount: ", accountBalance )
+		} else {
+			fmt.Println("Exiting the Bank. Thank You! " )
+		}
 	}
 }
